f003b18fbaaa8efdd6997747680759f02e5db40a
Haskkor <haskkor@gmail.com>
2017-05-14 13:01:02 +0000

    Implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
-  def hello
-    render html: "TwitterApp bid thee welcome"
-  end
+  include SessionsHelper
 end
